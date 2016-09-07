3700f71dbc23157e029be47a94a0002038f1e5b4
civir <manuiba.p500@gmail.com>
2016-09-07 12:35:02 +0200

    Add Example42/network

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -2,4 +2,19 @@ class profile::base {
 
   #the base profile should include component modules that will be on all nodes
 
+  class { '::ntp':
+  servers => [ 'ntp1.corp.com', 'ntp2.corp.com' ],
+  }
+
+  class { 'netowork': 
+    config_file_nofify => '',
+  } 
+ 
+  network::mroute { 'bond2':
+  routes => {
+    '192.168.2.0/24' => '192.168.1.1',
+    '80.81.82.0/16'  => 'eth0',
+    }  
+  }
+
 }
